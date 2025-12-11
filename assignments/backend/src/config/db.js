d654d9c0461166fcaf2fde3b8af3f4731e28311c
dhinadts <dhinadts@gmail.com>
2025-12-11 16:17:54 +0530

    POSTGRESQL Changes

--- assignments/backend/src/config/db.js
+++ assignments/backend/src/config/db.js
@@ -1,7 +1,9 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: "postgresql://dhinakaran_postgresql_user:V0PsAFHYYWVYE1stTMXwOJsfU5AdgTYu@dpg-d4t9ucchg0os73cpdvvg-a/dhinakaran_postgresql" ,
+      ssl: { rejectUnauthorized: false }
+
 });
 
 module.exports = pool;
